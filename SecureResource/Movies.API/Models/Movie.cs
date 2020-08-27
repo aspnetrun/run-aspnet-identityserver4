4395553942c516e7cf8153539134397f929ea3a7
Mehmet Ozkaya <mehmet.ozkaya@ericsson.com>
2020-08-27 13:34:49 +0300

    Movie added

---
+++ SecureResource/Movies.API/Models/Movie.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Movies.API.Models
+{
+    public class Movie
+    {
+        public int Id { get; set; }                
+        public string Title { get; set; }                
+        public string Genre { get; set; }        
+        public DateTime ReleaseDate { get; set; }
+        public string Owner { get; set; }
+    }
+}
