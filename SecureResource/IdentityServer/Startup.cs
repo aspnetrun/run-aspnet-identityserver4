be58a592383777441af3be20f1aa8c97b307dcfd
Mehmet Ozkaya <mehmet.ozkaya@ericsson.com>
2020-08-27 17:45:54 +0300

    add di

--- SecureResource/IdentityServer/Startup.cs
+++ SecureResource/IdentityServer/Startup.cs
@@ -16,6 +16,7 @@ namespace IdentityServer
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddIdentityServer();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -27,6 +28,7 @@ namespace IdentityServer
             }
 
             app.UseRouting();
+            app.UseIdentityServer();
 
             app.UseEndpoints(endpoints =>
             {
