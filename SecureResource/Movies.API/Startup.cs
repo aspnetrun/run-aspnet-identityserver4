53a97709997e1aecbb5eecf59afdab7ca926d740
Mehmet Ozkaya <mehmet.ozkaya@ericsson.com>
2020-08-27 14:41:40 +0300

    Use In-Memory Database

--- SecureResource/Movies.API/Startup.cs
+++ SecureResource/Movies.API/Startup.cs
@@ -30,7 +30,7 @@ namespace Movies.API
             services.AddControllers();
 
             services.AddDbContext<MoviesAPIContext>(options =>
-                    options.UseSqlServer(Configuration.GetConnectionString("MoviesAPIContext")));
+                    options.UseInMemoryDatabase("Movies"));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
