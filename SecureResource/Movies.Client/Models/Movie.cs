8e7468691e3b5154e284fff8211236d245276b70
Mehmet Ozkaya <mehmetozkaya@gmail.com>
2020-09-03 17:53:47 +0300

    added model class

---
+++ SecureResource/Movies.Client/Models/Movie.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Movies.Client.Models
+{
+    public class Movie
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Genre { get; set; }
+        public string Rating { get; set; }
+        public DateTime ReleaseDate { get; set; }
+        public string ImageUrl { get; set; }
+        public string Owner { get; set; }
+    }
+}
