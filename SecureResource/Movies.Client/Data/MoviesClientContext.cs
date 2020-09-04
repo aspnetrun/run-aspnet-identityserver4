907ca4c54534e8bab1c072611274882a1c356dc0
Mehmet Ozkaya <mehmetozkaya@gmail.com>
2020-09-04 13:18:11 +0300

    generate Movie code

---
+++ SecureResource/Movies.Client/Data/MoviesClientContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Movies.Client.Models;
+
+namespace Movies.Client.Data
+{
+    public class MoviesClientContext : DbContext
+    {
+        public MoviesClientContext (DbContextOptions<MoviesClientContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Movies.Client.Models.Movie> Movie { get; set; }
+    }
+}
