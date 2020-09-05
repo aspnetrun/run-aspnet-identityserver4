b065f1dd0b7f5c93d32d2d7c7a4c007d4fef2866
Mehmet Ozkaya <mehmetozkaya@gmail.com>
2020-09-05 12:47:11 +0300

    new client added

--- SecureResource/IdentityServer/Config.cs
+++ SecureResource/IdentityServer/Config.cs
@@ -1,4 +1,6 @@
-﻿using IdentityServer4.Models;
+﻿using IdentityModel;
+using IdentityServer4;
+using IdentityServer4.Models;
 using IdentityServer4.Test;
 using System.Collections.Generic;
 using System.Security.Claims;
@@ -20,6 +22,30 @@ namespace IdentityServer
                             new Secret("secret".Sha256())
                         },
                         AllowedScopes = { "movieAPI" }
+                   },
+                   new Client
+                   {
+                       ClientId = "movies_client",
+                       ClientName = "Movies MVC Web App",
+                       AllowedGrantTypes = GrantTypes.Code,
+                       AllowRememberConsent = false,
+                       RedirectUris = new List<string>()
+                       {
+                           "https://localhost:5002/signin-oidc"
+                       },
+                       PostLogoutRedirectUris = new List<string>()
+                       {
+                           "https://localhost:5002/signout-callback-oidc"
+                       },
+                       ClientSecrets = new List<Secret>
+                       {
+                           new Secret("secret".Sha256())
+                       },
+                       AllowedScopes = new List<string>
+                       {
+                           IdentityServerConstants.StandardScopes.OpenId,
+                           IdentityServerConstants.StandardScopes.Profile
+                       }
                    }
             };
 
@@ -36,12 +62,25 @@ namespace IdentityServer
 
         public static IEnumerable<IdentityResource> IdentityResources =>
           new IdentityResource[]
-          {              
+          {
+              new IdentityResources.OpenId(),
+              new IdentityResources.Profile()
           };             
 
         public static List<TestUser> TestUsers =>
             new List<TestUser>
             {
+                new TestUser
+                {
+                    SubjectId = "5BE86359-073C-434B-AD2D-A3932222DABE",
+                    Username = "mehmet",
+                    Password = "mehmet",
+                    Claims = new List<Claim>
+                    {
+                        new Claim(JwtClaimTypes.GivenName, "mehmet"),
+                        new Claim(JwtClaimTypes.FamilyName, "ozkaya")
+                    }
+                }
             };
     }
 }
