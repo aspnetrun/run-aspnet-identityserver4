7a6d5128cef10ad6afb49527f482aa9b8f636dff
Mehmet Ozkaya <mehmetozkaya@gmail.com>
2020-09-05 12:51:17 +0300

    set di

--- SecureResource/IdentityServer/Startup.cs
+++ SecureResource/IdentityServer/Startup.cs
@@ -23,6 +23,8 @@ namespace IdentityServer
             services.AddIdentityServer()
                 .AddInMemoryClients(Config.Clients)
                 .AddInMemoryApiScopes(Config.ApiScopes)
+                .AddInMemoryIdentityResources(Config.IdentityResources)
+                .AddTestUsers(Config.TestUsers)
                 .AddDeveloperSigningCredential();
         }
 
