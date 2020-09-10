3e321d2c792229a8f60b2e7938fdb525a593d0de
Mehmet Ozkaya <mehmetozkaya@gmail.com>
2020-09-10 14:34:54 +0300

    IdentityApiKey

--- SecureResource/ApiGateway/Startup.cs
+++ SecureResource/ApiGateway/Startup.cs
@@ -7,6 +7,7 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Microsoft.IdentityModel.Tokens;
 using Ocelot.DependencyInjection;
 using Ocelot.Middleware;
 
@@ -18,6 +19,20 @@ namespace ApiGateway
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            var authenticationProviderKey = "IdentityApiKey";
+
+            // NUGET - Microsoft.AspNetCore.Authentication.JwtBearer
+            services.AddAuthentication()
+             .AddJwtBearer(authenticationProviderKey, x =>
+             {
+                 x.Authority = "https://localhost:5005"; // IDENTITY SERVER URL
+                 x.RequireHttpsMetadata = false;
+                 x.TokenValidationParameters = new TokenValidationParameters
+                 {
+                     ValidateAudience = false
+                 };
+             });
+
             services.AddOcelot();
         }
 
