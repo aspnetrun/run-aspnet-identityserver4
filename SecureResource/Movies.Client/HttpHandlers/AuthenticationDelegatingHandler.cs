5cd716e331d147730fc80b2abcc77b1c4255fb72
Mehmet Ozkaya <mehmetozkaya@gmail.com>
2020-09-11 09:53:37 +0300

    refactor

--- SecureResource/Movies.Client/HttpHandlers/AuthenticationDelegatingHandler.cs
+++ SecureResource/Movies.Client/HttpHandlers/AuthenticationDelegatingHandler.cs
@@ -3,8 +3,6 @@ using Microsoft.AspNetCore.Authentication;
 using Microsoft.AspNetCore.Http;
 using Microsoft.IdentityModel.Protocols.OpenIdConnect;
 using System;
-using System.Collections.Generic;
-using System.Linq;
 using System.Net.Http;
 using System.Threading;
 using System.Threading.Tasks;
@@ -47,7 +45,7 @@ namespace Movies.Client.HttpHandlers
             if (!string.IsNullOrWhiteSpace(accessToken))
             {
                 request.SetBearerToken(accessToken);
-            }            
+            }
 
             return await base.SendAsync(request, cancellationToken);
         }
