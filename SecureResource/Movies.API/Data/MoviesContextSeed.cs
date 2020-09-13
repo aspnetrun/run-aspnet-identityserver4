9b94d5ade764b302e838b48a46d95632d189b7a4
Mehmet Ozkaya <mehmetozkaya@gmail.com>
2020-09-13 13:52:44 +0300

    seeding

--- SecureResource/Movies.API/Data/MoviesContextSeed.cs
+++ SecureResource/Movies.API/Data/MoviesContextSeed.cs
@@ -18,12 +18,82 @@ namespace Movies.API.Data
                     new Movie
                     {
                         Id = 1,
-                        Genre = "Comics",
-                        Title = "asd",
+                        Genre = "Drama",
+                        Title = "The Shawshank Redemption",
+                        Rating = "9.3",
+                        ImageUrl = "images/src",
+                        ReleaseDate = new DateTime(1994, 5, 5),
+                        Owner = "alice"
+                    },
+                    new Movie
+                    {
+                        Id = 2,
+                        Genre = "Crime",
+                        Title = "The Godfather",
                         Rating = "9.2",
                         ImageUrl = "images/src",
-                        ReleaseDate = DateTime.Now,
-                        Owner = "swn"
+                        ReleaseDate = new DateTime(1972, 5, 5),
+                        Owner = "alice"
+                    },
+                    new Movie
+                    {
+                        Id = 3,
+                        Genre = "Action",
+                        Title = "The Dark Knight",
+                        Rating = "9.1",
+                        ImageUrl = "images/src",
+                        ReleaseDate = new DateTime(2008, 5, 5),
+                        Owner = "bob"
+                    },
+                    new Movie
+                    {
+                        Id = 4,
+                        Genre = "Crime",
+                        Title = "12 Angry Men",
+                        Rating = "8.9",
+                        ImageUrl = "images/src",
+                        ReleaseDate = new DateTime(1957, 5, 5),
+                        Owner = "bob"
+                    },
+                    new Movie
+                    {
+                        Id = 5,
+                        Genre = "Biography",
+                        Title = "Schindler's List",
+                        Rating = "8.9",
+                        ImageUrl = "images/src",
+                        ReleaseDate = new DateTime(1993, 5, 5),
+                        Owner = "alice"
+                    },
+                    new Movie
+                    {
+                        Id = 6,
+                        Genre = "Drama",
+                        Title = "Pulp Fiction",
+                        Rating = "8.9",
+                        ImageUrl = "images/src",
+                        ReleaseDate = new DateTime(1994, 5, 5),
+                        Owner = "alice"
+                    },
+                    new Movie
+                    {
+                        Id = 7,
+                        Genre = "Drama",
+                        Title = "Fight Club",
+                        Rating = "8.8",
+                        ImageUrl = "images/src",
+                        ReleaseDate = new DateTime(1999, 5, 5),
+                        Owner = "bob"
+                    },
+                    new Movie
+                    {
+                        Id = 8,
+                        Genre = "Romance",
+                        Title = "Forrest Gump",
+                        Rating = "8.8",
+                        ImageUrl = "images/src",
+                        ReleaseDate = new DateTime(1994, 5, 5),
+                        Owner = "bob"
                     }
                 };
                 moviesContext.Movie.AddRange(movies);
