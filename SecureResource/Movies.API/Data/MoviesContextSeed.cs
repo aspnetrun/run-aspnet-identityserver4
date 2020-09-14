cef1dacad36682b3f2623aaa711d53d27c094b72
Mehmet Ozkaya <mehmetozkaya@gmail.com>
2020-09-14 09:34:32 +0300

    refactored

--- SecureResource/Movies.API/Data/MoviesContextSeed.cs
+++ SecureResource/Movies.API/Data/MoviesContextSeed.cs
@@ -1,9 +1,7 @@
-﻿using Microsoft.EntityFrameworkCore;
-using Movies.API.Models;
+﻿using Movies.API.Models;
 using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Threading.Tasks;
 
 namespace Movies.API.Data
 {
